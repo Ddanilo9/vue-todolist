c755365da29473ab3fa358de1707e67c654b26d2
Ddanilo9 <interiordanilo3@gmail.com>
2022-08-25 14:34:19 +0200

    adding new task

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const todos = [
     },
     {
         text:'Fare la doccia',
-        done: true
+        done: false
     },
     {
         text:'Dare da mangiare al cane',
@@ -21,8 +21,23 @@ const app = new Vue ({
     el: '#app',
     data: {
         todos: todos,
+        newTask: '',
     },
     methods: {
-       
+       isDoneOnOff(todo){
+        todo.done = !todo.done
+       },
+       addNewTask(){
+        if (this.newTask != this.newTask.length<0){
+                        
+            this.todos.push({
+                text: this.newTask,
+                done: false
+            })
+
+            this.newTask = ''
+        }
+        
+       }
     },
 })
